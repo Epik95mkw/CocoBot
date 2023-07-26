4e9b1ce5b6acfd86d995f4e866d1c4bf4952a524
Epik95mkw <epik955@gmail.com>
2023-07-26 11:31:58 -0400

    Adjust timing of scheduled functions

--- components/scheduled.py
+++ components/scheduled.py
@@ -8,13 +8,17 @@ class ScheduledFunctions(commands.Cog):
     def __init__(self, _bot: commands.Bot):
         self.bot = _bot
         self.on_map_rotation.start()
+        self.check_patch.start()
 
     def cog_unload(self):
         self.on_map_rotation.cancel()
+        self.check_patch.cancel()
 
-    @tasks.loop(time=[datetime.time(hour=t, second=30) for t in range(0, 24, 2)])
+    @tasks.loop(time=[datetime.time(hour=t, second=30) for t in range(0, 24, 1)])
     async def on_map_rotation(self):
-        guilds = list(self.bot.guilds)
-        await f.update_maps(guilds)
-        await f.check_new_patch(guilds)
-        await f.update_gear(guilds)
+        await f.update_maps(list(self.bot.guilds))
+        await f.update_gear(list(self.bot.guilds))
+
+    @tasks.loop(time=[datetime.time(hour=t, minute=m, second=30) for m in range(2, 12, 2) for t in range(0, 24, 1)])
+    async def check_patch(self):
+        await f.check_new_patch(list(self.bot.guilds))
